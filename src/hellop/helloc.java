f639919d25facb2eb2f26dff0a9853c68f131cde
sstbmj <65929832+sstbmj@users.noreply.github.com>
2020-09-24 14:04:11 +0900

    Update helloc.java

--- src/hellop/helloc.java
+++ src/hellop/helloc.java
@@ -5,7 +5,7 @@ public class helloc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello java");
-		System.out.println("zzz");
+		System.out.println("git");
 	}
 
 }
