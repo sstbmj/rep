c6fc6a22333197f702e4620b145ce7f6060ce77a
stshin <stshin@245415-01.poscoict.ad>
2020-09-24 13:30:31 +0900

    first commit

---
+++ src/hellop/helloc.java
@@ -0,0 +1,11 @@
+package hellop;
+
+public class helloc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java");
+		System.out.println("zzz");
+	}
+
+}
